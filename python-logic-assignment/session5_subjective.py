463815e6cf17b4d6c16f8667ffcb745d4baea0e4
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-05 12:06:29 +0530

    Session 5 assignment subjective

---
+++ python-logic-assignment/session5_subjective.py
@@ -0,0 +1,5 @@
+marks = int(input("Enter your makrs :"))  # Int --> Integer, input() --> is fr taking the data from the user
+if marks >= 50 :                            # If marks >= 50 , student passed
+    print("Student passed")
+else:
+    print("Student Failed")                 # if marks < 50, student failed.
